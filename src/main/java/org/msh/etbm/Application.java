859476b05d0bdec3bab86919463f3ef51ec8c4a9
Amir <61427423+Amir-UL-Islam@users.noreply.github.com>
2023-12-12 13:59:01 +0600

    In a State

--- src/main/java/org/msh/etbm/Application.java
+++ src/main/java/org/msh/etbm/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
